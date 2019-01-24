b80fad5c611d99134951e244df9fa354fdad9ed9
ponlia <lia.pon@mail.utoronto.ca>
2019-01-23 16:59:20 -0800

    Updates

---
+++ precache-manifest.4c512550741f6b037d0890894a3007cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mephobia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fc48e3149a9e8d6f4dc5",
+    "url": "/mephobia/static/js/runtime~main.fc48e314.js"
+  },
+  {
+    "revision": "85ccd6d70d0689e4e7a2",
+    "url": "/mephobia/static/js/main.85ccd6d7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mephobia/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "85ccd6d70d0689e4e7a2",
+    "url": "/mephobia/static/css/main.e3f38597.chunk.css"
+  },
+  {
+    "revision": "1143526f164104589e2f69d25f5b6ed7",
+    "url": "/mephobia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mephobia-react-app/precache-manifest.757a26b73b78db811cccc661e646fd69.js"
+  "/mephobia/precache-manifest.4c512550741f6b037d0890894a3007cd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mephobia-react-app/index.html", {
+workbox.routing.registerNavigationRoute("/mephobia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

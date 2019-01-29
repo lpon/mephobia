d301ebdfad23ded3553d220fe5b5e9eb1a49b5ee
ponlia <lia.pon@mail.utoronto.ca>
2019-01-28 16:59:11 -0800

    Updates

---
+++ precache-manifest.0aee3a33793463e5f26b532cb399aaa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc48e3149a9e8d6f4dc5",
+    "url": "/mephobia/static/js/runtime~main.fc48e314.js"
+  },
+  {
+    "revision": "c3278c71a2fa577b7698",
+    "url": "/mephobia/static/js/main.c3278c71.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/mephobia/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c3278c71a2fa577b7698",
+    "url": "/mephobia/static/css/main.ef0edcc3.chunk.css"
+  },
+  {
+    "revision": "0488e1ed417eb6c0bb9d73f3932f86c3",
+    "url": "/mephobia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mephobia/precache-manifest.8a83950754402dac7de570920f01d140.js"
+  "/mephobia/precache-manifest.0aee3a33793463e5f26b532cb399aaa8.js"
 );
 
 workbox.clientsClaim();

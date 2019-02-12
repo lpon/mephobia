507e628142f1f1c71a77976f0e70107d73926616
ponlia <lia.pon@mail.utoronto.ca>
2019-02-11 18:21:55 -0800

    added borders to images on homepage and music page

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import homepageImage from '../../assets/homepage.png';
+import '../../stylesheets/Home.css';
 
 
 class Home extends Component {
@@ -7,7 +8,7 @@ class Home extends Component {
         return (
             <div>
                 <h1 className="pageHeader">〜(￣▽￣〜)ヾ(*д*)ﾉ゛(〜￣▽￣)〜</h1>
-                <img src={homepageImage}></img>
+                <img className="homepageImage" src={homepageImage}></img>
             </div>
         );
     }

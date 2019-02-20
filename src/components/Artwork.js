2dc983794f71593d63d25af96d8a30552852a43d
ponlia <lia.pon@mail.utoronto.ca>
2019-02-20 01:58:20 -0800

    added new drawing

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,21 +1,22 @@
-import artwork0 from "../assets/artwork/Anakin.jpg";
-import artwork1 from "../assets/artwork/AnnaFischer.jpg";
-import artwork2 from "../assets/artwork/Deadpool.jpg";
-import artwork3 from "../assets/artwork/Joker.jpg";
-import artwork4 from "../assets/artwork/LIghtbulb.jpg";
-import artwork5 from "../assets/artwork/Rorschach.JPG";
-import artwork6 from "../assets/artwork/SailorMoon_021919.png";
-
+import artwork0 from "../../assets/artwork/Anakin.jpg";
+import artwork1 from "../../assets/artwork/AnnaFischer.jpg";
+import artwork2 from "../../assets/artwork/Aoi_021919.png";
+import artwork3 from "../../assets/artwork/Deadpool.jpg";
+import artwork4 from "../../assets/artwork/Joker.jpg";
+import artwork5 from "../../assets/artwork/LIghtbulb.jpg";
+import artwork6 from "../../assets/artwork/Rorschach.JPG";
+import artwork7 from "../../assets/artwork/SailorMoon_021919.png";
 
 
 const artwork = [
-    {src: artwork6, caption: "SailorMoon_021919.png", width: 1766, height: 2356},
-    {src: artwork0, caption: "Anakin.jpg", width: 3641, height: 2568},
-    {src: artwork1, caption: "AnnaFischer.jpg", width: 2679, height: 3456},
-    {src: artwork2, caption: "Deadpool.jpg", width: 1023, height: 1362},
-    {src: artwork3, caption: "Joker.jpg", width: 500, height: 672},
-    {src: artwork4, caption: "LIghtbulb.jpg", width: 4157, height: 2771},
-    {src: artwork5, caption: "Rorschach.JPG", width: 3456, height: 5184},
+    {src: artwork7, caption: "SailorMoon_021919.png", width: 1750, height: 2334},
+    {src: artwork0, caption: "Anakin.jpg", width: 1750, height: 2334},
+    {src: artwork6, caption: "Rorschach.JPG", width: 1750, height: 2334},
+    {src: artwork1, caption: "AnnaFischer.jpg", width: 1750, height: 2334},
+    {src: artwork2, caption: "Aoi_021919.png", width: 1750, height: 2334},
+    {src: artwork3, caption: "Deadpool.jpg", width: 1750, height: 2334},
+    {src: artwork4, caption: "Joker.jpg", width: 1750, height: 2334},
+    {src: artwork5, caption: "LIghtbulb.jpg", width: 1750, height: 2334},
 ];
 
 export default artwork;

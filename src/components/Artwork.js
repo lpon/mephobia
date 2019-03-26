32a232ce58ea19c01fc516cabdfd3037f508beea
ponlia <lia.pon@mail.utoronto.ca>
2019-03-26 16:48:05 -0700

    removed a piece of artwork

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,6 +1,5 @@
 import artwork0 from "../assets/artwork/Anakin.jpg";
 import artwork1 from "../assets/artwork/AnnaFischer.jpg";
-import artwork2 from "../assets/artwork/Aoi_021919.png";
 import artwork3 from "../assets/artwork/Deadpool.jpg";
 import artwork4 from "../assets/artwork/Joker.jpg";
 import artwork5 from "../assets/artwork/LIghtbulb.jpg";
@@ -13,7 +12,6 @@ const artwork = [
     {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
     {src: artwork6, caption: "Rorschach.JPG", width: 2765, height: 4147},
     {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
-    {src: artwork2, caption: "Aoi_021919.png", width: 1944, height: 2588},
     {src: artwork3, caption: "Deadpool.jpg", width: 818, height: 1090},
     {src: artwork4, caption: "Joker.jpg", width: 400, height: 538},
     {src: artwork5, caption: "LIghtbulb.jpg", width: 3326, height: 2217},

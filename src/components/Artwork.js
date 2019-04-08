5f7d5e20278789688f2f3a8fea8b8284e7ba93ca
ponlia <lia.pon@mail.utoronto.ca>
2019-04-08 16:25:35 -0700

    reordered artwork on page

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -10,16 +10,16 @@ import artwork9 from "../assets/artwork/SadGirl_040619.png";
 import artwork10 from "../assets/artwork/SailorMoon_021919.png";
 
 const artwork = [
-    {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
-    {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
+    {src: artwork8, caption: "Rorschach.JPG", width: 2765, height: 4147},
     {src: artwork3, caption: "ChineseGirl_040819.png", width: 2676, height: 2006},
-    {src: artwork4, caption: "Deadpool.jpg", width: 818, height: 1090},
+    {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
+    {src: artwork9, caption: "SadGirl_040619.png", width: 1787, height: 2384},
     {src: artwork5, caption: "Joker.jpg", width: 400, height: 538},
+    {src: artwork10, caption: "SailorMoon_021919.png", width: 1750, height: 2334},  
     {src: artwork6, caption: "LIghtbulb.jpg", width: 3326, height: 2217},
     {src: artwork7, caption: "PinkHairedGirl_032719.png", width: 1942, height: 2590},
-    {src: artwork8, caption: "Rorschach.JPG", width: 2765, height: 4147},
-    {src: artwork9, caption: "SadGirl_040619.png", width: 1787, height: 2384},
-    {src: artwork10, caption: "SailorMoon_021919.png", width: 1750, height: 2334},  
+    {src: artwork4, caption: "Deadpool.jpg", width: 818, height: 1090},
+    {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
 ];
 
 

9823e1c0a7ffd7cac5685db8bc9c387402c0c9cc
ponlia <lia.pon@mail.utoronto.ca>
2019-04-11 12:59:00 -0700

    updated artwork

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,26 +1,29 @@
 import artwork0 from "../assets/artwork/Anakin.jpg";
 import artwork1 from "../assets/artwork/AnnaFischer.jpg";
-import artwork3 from "../assets/artwork/ChineseGirl_040819.png";
-import artwork4 from "../assets/artwork/Deadpool.jpg";
-import artwork5 from "../assets/artwork/Joker.jpg";
-import artwork6 from "../assets/artwork/LIghtbulb.jpg";
+import artwork2 from "../assets/artwork/ChineseGirl_040819.png";
+import artwork3 from "../assets/artwork/Deadpool.jpg";
+import artwork4 from "../assets/artwork/Joker.jpg";
+import artwork5 from "../assets/artwork/LIghtbulb.jpg";
+import artwork6 from "../assets/artwork/LuVan_041119.png";
 import artwork7 from "../assets/artwork/PinkHairedGirl_032719.png";
 import artwork8 from "../assets/artwork/Rorschach.JPG";
 import artwork9 from "../assets/artwork/SadGirl_040619.png";
 import artwork10 from "../assets/artwork/SailorMoon_021919.png";
 
 const artwork = [
-    {src: artwork8, caption: "Rorschach.JPG", width: 2765, height: 4147},
-    {src: artwork3, caption: "ChineseGirl_040819.png", width: 2676, height: 2006},
+    {src: artwork6, caption: "LuVan_041119.png", width: 2536, height: 1900},
     {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
     {src: artwork9, caption: "SadGirl_040619.png", width: 1787, height: 2384},
-    {src: artwork5, caption: "Joker.jpg", width: 400, height: 538},
-    {src: artwork10, caption: "SailorMoon_021919.png", width: 1750, height: 2334},  
-    {src: artwork6, caption: "LIghtbulb.jpg", width: 3326, height: 2217},
-    {src: artwork7, caption: "PinkHairedGirl_032719.png", width: 1942, height: 2590},
-    {src: artwork4, caption: "Deadpool.jpg", width: 818, height: 1090},
     {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
+    {src: artwork3, caption: "Deadpool.jpg", width: 818, height: 1090},
+    {src: artwork10, caption: "SailorMoon_021919.png", width: 1750, height: 2334},
+    {src: artwork4, caption: "Joker.jpg", width: 400, height: 538},
+    {src: artwork5, caption: "LIghtbulb.jpg", width: 3326, height: 2217},
+    {src: artwork2, caption: "ChineseGirl_040819.png", width: 2676, height: 2006},
+    {src: artwork8, caption: "Rorschach.JPG", width: 2765, height: 4147},
+    {src: artwork7, caption: "PinkHairedGirl_032719.png", width: 1942, height: 2590},
 ];
+
 
 
 

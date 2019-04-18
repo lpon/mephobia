395d852bc52f61645e0cd3513e002ece3c1045c7
ponlia <lia.pon@mail.utoronto.ca>
2019-04-17 17:47:45 -0700

    updated artwork

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,31 +1,30 @@
 import artwork0 from "../assets/artwork/Anakin.jpg";
 import artwork1 from "../assets/artwork/AnnaFischer.jpg";
-import artwork3 from "../assets/artwork/Deadpool.jpg";
-import artwork4 from "../assets/artwork/Joker.jpg";
-import artwork5 from "../assets/artwork/LIghtbulb.jpg";
-import artwork6 from "../assets/artwork/LuVan_041119.png";
+import artwork2 from "../assets/artwork/Deadpool.jpg";
+import artwork3 from "../assets/artwork/Joker.jpg";
+import artwork4 from "../assets/artwork/LIghtbulb.jpg";
+import artwork5 from "../assets/artwork/LuVan_041119.png";
+import artwork6 from "../assets/artwork/Me_021919.png";
 import artwork7 from "../assets/artwork/PinkHairedGirl_032719.png";
 import artwork8 from "../assets/artwork/Rorschach.JPG";
 import artwork9 from "../assets/artwork/SadGirl_040619.png";
 import artwork10 from "../assets/artwork/SailorMoon_021919.png";
 
+
+
 const artwork = [
-    {src: artwork9, caption: "SadGirl_040619.png", width: 1787, height: 2384},
-    {src: artwork4, caption: "Joker.jpg", width: 400, height: 538},
-    {src: artwork6, caption: "LuVan_041119.png", width: 2536, height: 1900},
-    {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
-    {src: artwork3, caption: "Deadpool.jpg", width: 818, height: 1090},
-    {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
-    {src: artwork10, caption: "SailorMoon_021919.png", width: 1750, height: 2334},
-    {src: artwork5, caption: "LIghtbulb.jpg", width: 3326, height: 2217},
+    {src: artwork5, caption: "LuVan_041119.png", width: 2424, height: 1816},
     {src: artwork8, caption: "Rorschach.JPG", width: 2765, height: 4147},
+    {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
+    {src: artwork4, caption: "LIghtbulb.jpg", width: 3326, height: 2217},
+    {src: artwork10, caption: "SailorMoon_021919.png", width: 1750, height: 2334},
+    {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
+    {src: artwork2, caption: "Deadpool.jpg", width: 818, height: 1090},
+    {src: artwork6, caption: "Me_021919.png", width: 1759, height: 2346},
+    {src: artwork3, caption: "Joker.jpg", width: 400, height: 538},
     {src: artwork7, caption: "PinkHairedGirl_032719.png", width: 1942, height: 2590},
+    // {src: artwork9, caption: "SadGirl_040619.png", width: 1787, height: 2384},
 ];
-
-
-
-
-
 
 
 export default artwork;

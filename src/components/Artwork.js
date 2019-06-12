53661f8f66b50440c1c476ed69fb29e511f9c9ea
ponlia <lia.pon@mail.utoronto.ca>
2019-06-11 19:36:23 -0700

    added artwork

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -6,27 +6,28 @@ import artwork4 from "../assets/artwork/LIghtbulb.jpg";
 import artwork5 from "../assets/artwork/LuVan_041119.png";
 import artwork6 from "../assets/artwork/Me_021919.png";
 import artwork7 from "../assets/artwork/MingXi_041919.png";
-import artwork8 from "../assets/artwork/PinkHairedGirl_032719.png";
 import artwork9 from "../assets/artwork/Rorschach.JPG";
-import artwork10 from "../assets/artwork/SadGirl_040619.png";
 import artwork11 from "../assets/artwork/SailorMoon_021919.png";
-import artwork12 from "../assets/artwork/fantasyGirl_050119.png";
-import artwork13 from "../assets/artwork/quietMoment_050619.png";
+import artwork12 from "../assets/artwork/WetHair_061119.png";
+import artwork13 from "../assets/artwork/fantasyGirl_050119.png";
+import artwork14 from "../assets/artwork/quietMoment_050619.png";
 
 const artwork = [
-    {src: artwork13, caption: "quietMoment_050619.png", width: 2732, height: 2048},    
     {src: artwork0, caption: "Anakin.jpg", width: 2913, height: 2054},
     {src: artwork1, caption: "AnnaFischer.jpg", width: 2143, height: 2765},
-    {src: artwork5, caption: "LuVan_041119.png", width: 2337, height: 1728},
     {src: artwork2, caption: "Deadpool.jpg", width: 818, height: 1090},
-    {src: artwork7, caption: "MingXi_041919.png", width: 1815, height: 2421},
     {src: artwork3, caption: "Joker.jpg", width: 400, height: 538},
-    {src: artwork6, caption: "Me_021919.png", width: 1759, height: 2346},
-    {src: artwork11, caption: "SailorMoon_021919.png", width: 1750, height: 2334},
     {src: artwork4, caption: "LIghtbulb.jpg", width: 3326, height: 2217},
-    {src: artwork12, caption: "fantasyGirl_050119.png", width: 2048, height: 2732},
+    {src: artwork5, caption: "LuVan_041119.png", width: 2337, height: 1728},
+    {src: artwork6, caption: "Me_021919.png", width: 1759, height: 2346},
+    {src: artwork7, caption: "MingXi_041919.png", width: 1815, height: 2421},
     {src: artwork9, caption: "Rorschach.JPG", width: 2765, height: 4147},
+    {src: artwork11, caption: "SailorMoon_021919.png", width: 1750, height: 2334},
+    {src: artwork12, caption: "WetHair_061119.png", width: 1832, height: 2443},
+    {src: artwork13, caption: "fantasyGirl_050119.png", width: 2048, height: 2732},
+    {src: artwork14, caption: "quietMoment_050619.png", width: 2732, height: 2048},
     ];
+
 
 
 
